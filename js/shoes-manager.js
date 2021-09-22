dab352499d7b12b6c1fa80f1456caf73fda9c041
hamasakij <j.hamasaki@wiz-net.jp>
2021-09-22 12:00:20 +0900

    no message

--- js/shoes-manager.js
+++ js/shoes-manager.js
@@ -5,11 +5,22 @@ $(document).ready(function () {
     // モーダル表示
     $(".ui.modal.shipping-modal").modal("show");
   });
-});
-$(document).ready(function () {
+
   // 更新ボタン押下時
   $(".inverted.button").click(function () {
     // モーダル表示
     $(".ui.modal.update-modal").modal("show");
+  });
+
+  $("#date_calendar").calendar({
+    type: "date",
+    formatter: {
+      date: function (date) {
+        var day = ("0" + date.getDate()).slice(-2);
+        var month = ("0" + (date.getMonth() + 1)).slice(-2);
+        var year = date.getFullYear();
+        return year + "/" + month + "/" + day;
+      },
+    },
   });
 });
